1dc9ded5266b547eb0ff6874a032d82eb021fcfa
Sreehitha <sreehithasharma14@gmail.com>
2026-02-25 18:46:30 +0530

    Create model_api.py

---
+++ Milestone 2/model_api.py
@@ -0,0 +1,26 @@
+from huggingface_hub import InferenceClient
+import os
+
+def query_model(prompt):
+    try:
+        HF_TOKEN = os.getenv("HF_TOKEN")
+
+        client = InferenceClient(
+            model="mistralai/Mistral-7B-Instruct-v0.2",
+            token=HF_TOKEN
+        )
+
+        response = client.chat_completion(
+            messages=[
+                {"role": "system", "content": "You are a certified professional fitness trainer."},
+                {"role": "user", "content": prompt}
+            ],
+            max_tokens=2400,      # increase slightly
+            temperature=0.6,      # lower randomness
+            top_p=0.9
+        )
+
+        return response.choices[0].message.content
+
+    except Exception as e:
+        return f"Error: {str(e)}"
